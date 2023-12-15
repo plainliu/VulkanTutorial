787ca28331417c138b314e5eafcfd6936763e0d6
liujuanjuan <liujuanjuan134@foxmail.com>
2023-12-15 19:02:21 +0800

    HelloTriangleApplication init

---
+++ src/HelloTriangleApplication.h
@@ -0,0 +1,27 @@
+#include <vulkan/vulkan.h>
+
+#include <iostream>
+#include <stdexcept>
+#include <cstdlib>
+
+class HelloTriangleApplication {
+public:
+    void run() {
+        initVulkan();
+        mainLoop();
+        cleanup();
+    }
+
+private:
+    void initVulkan() {
+
+    }
+
+    void mainLoop() {
+
+    }
+
+    void cleanup() {
+
+    }
+};
